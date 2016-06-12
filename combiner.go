184778fb0d9ff7dae3e1923d21531a088df85ccd
Lucy Linder <lucy.derlin@gmail.com>
2016-06-12 08:27:57 +0200

    add optional gap between images

--- combiner.go
+++ combiner.go
@@ -11,6 +11,7 @@ import (
 	"github.com/nfnt/resize"
 	"math"
 	"fmt"
+	"flag"
 )
 
 // Create a struct to deal with pixel
@@ -54,10 +55,31 @@ func DecodePixelsFromImage(img image.Image, offsetX, offsetY int) []*Pixel {
 	return pixels
 }
 
+func CreateGap(x int, yGap int, offsetY int, color color.Color) []*Pixel {
+	gapPixels := make([]*Pixel, x*yGap);
+	idx := 0
+	for i := offsetY; i < offsetY+yGap; i++ {
+		for j := 0; j < x; j++ {
+			gapPixels[idx] = &Pixel{
+				Point: image.Point{j, i},
+				Color: color,
+			}
+			idx++
+		}
+	}
+	fmt.Println(len(gapPixels))
+	return gapPixels
+}
+
 func main() {
 
-	image_paths := os.Args[1:];
+	var yGap int;
+	var yGapStrColor string;
+	flag.IntVar(&yGap, "gap", 0, "gap between images");
+	flag.IntVar(&yGapStrColor, "gapColor", 0, "color of gap between images");
+	flag.Parse();
 
+	image_paths := os.Args[len(os.Args) - flag.NArg():];
 	images := make([]image.Image, len(image_paths))
 	var pixels []*Pixel;
 	x := math.MaxInt32;
@@ -70,11 +92,17 @@ func main() {
 		images[idx] = img
 	}
 
+	fmt.Println(yGap)
+
 	offset := 0;
-	for _, img := range (images) {
+	for i, img := range (images) {
 		ResizeTo(&img, x)
 		pixels = append(pixels, DecodePixelsFromImage(img, 0, offset)...);
 		offset += img.Bounds().Max.Y;
+		if yGap > 0 && i < len(images) - 1 {
+			pixels = append(pixels, CreateGap(x, yGap, offset, color.RGBA{255,255,255,255})...)
+			offset += int(yGap);
+		}
 	}
 	pixelSum := append(pixels)
 	fmt.Print()
